99e01c7584c67a1cc15395fb649b476bb3f17143
Shubhradeep Roy <166045777+Shubhradee@users.noreply.github.com>
2025-03-25 00:32:39 +0530

    Delete QuestEd directory

--- QuestEd/src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-import LoginForm from '@/components/Auth/LoginForm';
-import '@/styles/globals.css';
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
-
--- QuestEd/src/app/(auth)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-import RegisterForm from '@/components/Auth/RegisterForm';
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
