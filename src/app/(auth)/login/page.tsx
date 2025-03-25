5f9020b1406d911a18bc583d59d44f0e5c546bf9
Shubhradeep Roy <166045777+Shubhradee@users.noreply.github.com>
2025-03-25 21:50:36 +0530

    Add files via upload

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic';
+
+import LoginForm from '@/components/Auth/LoginForm';
+import '@/styles/globals.css';
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
+
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import RegisterForm from '@/components/Auth/RegisterForm';
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
