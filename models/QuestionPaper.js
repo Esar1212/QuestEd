ee2fb9b8f01a344b877fbb4d2d34546aa685a968
Shubhradeep Roy <166045777+Esar1212@users.noreply.github.com>
2025-09-25 00:32:44 +0530

    Update QuestionPaper.js

--- models/QuestionPaper.js
+++ models/QuestionPaper.js
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
 
+const QuestionSchema = new mongoose.Schema({
+  type: { type: String, enum: ["mcq", "descriptive"], required: true },
+  question: { type: String, required: true },
+  // Only required for MCQ
+  options: [{ type: String }],
+  answer: { type: String, required: true },
+  marks: { type: Number, required: true }
+});
+
 const QuestionPaperSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subject: { type: String, required: true },
   timeLimit: { type: Number, required: true },
   totalMarks: { type: Number, required: true },
   classStream: { type: String, required: true },
-  questions: [
-    {
-      question: { type: String, required: true },
-      options: { type: [String], required: true },
-      answer: { type: String, required: true },
-      marks: { type: Number, required: true },
-    },
-  ],
-
-  createdAt: { type: Date, default: Date.now },
+  questions: [QuestionSchema] ,
+  createdAt: { type: Date, default: Date.now }
 });
 
 // Check if model exists before defining it again (prevents OverwriteModelError)
